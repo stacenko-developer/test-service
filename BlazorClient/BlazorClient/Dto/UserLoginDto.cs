6242875f029ff61830a9067bd65362a65d0e27e3
Alexey Mishkin <82732974+lexxamcode@users.noreply.github.com>
2024-10-17 13:34:25 +0400

    Инициализация клиента (#1)
    * Инициализация клиента
    
    * Базовый лайаут
    
    * Главная страница, стилизация, роутинг в авторизацию
    
    * Добавление авторизации в клиент
    
    * Удаление лишнего мейнлайаута
    
    * Модификация login/logout

---
+++ BlazorClient/BlazorClient/Dto/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorClient.Dto;
+
+public class UserLoginDto
+{
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public int UserOffsetSecond { get; set; } = 14400;
+}
